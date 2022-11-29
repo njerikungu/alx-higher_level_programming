a562b0286299ab570a99e973255842034bb20479
njerikungu <kungumaryanne@gmail.com>
2022-11-29 10:45:04 -0800

    lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letters in range(97, 123):
-    if chr(letters) != 101 and chr(letters) != 123:
-        print("{}".format(chr(letters)), end="")
+for ch in range(97, 123):
+    if ch != 101 and ch != 123:
+        print("{:c}".format(ch), end="")
