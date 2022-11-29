1c6e938610719c931214d98f0840f8e2cfa2c8a0
njerikungu <kungumaryanne@gmail.com>
2022-11-29 12:07:37 -0800

    new commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for num in range(0, 90):
-        if num % 10 > num / 10:
-                if num != 89:
-                        print("{:02d}, ".format(num), end='')
-                else:
-                        print("{:02d}".format(num))
+for num1 in range(0, 10):
+    for num2 in range((num1+1), 10):
+        if (num1 is not 8) or (num2 is not 9):
+            print("{}{}, ".format(num1, num2), end="")
+        else:
+            print("{}{}".format(num1, num2))
+
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    for num in range(97, 123):
+        if ord(c) == num:
+            return True
+        else:
+            return False
