0af9342dd9512a0b7257f14429b7e05c88d14f76
njerikungu <kungumaryanne@gmail.com>
2022-11-29 12:47:51 -0800

    corrections

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end="")
+for i in range(ord('z'), ord('a')-1, -1):
+    print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
