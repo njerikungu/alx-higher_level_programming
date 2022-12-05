73e727f01673a73e6e3d05c78dcabd313acc883d
njerikungu <kungumaryanne@gmail.com>
2022-12-05 13:32:22 -0800

    biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+
+    if length == 0:
+        return (None)
+
+    big = my_list[0]
+
+    for i in range(1, length):
+        if my_list[i] > big:
+            big = my_list[i]
+
+            return (big)
