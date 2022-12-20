71f29e302ddc8d638836d46b425bba92612fd440
njerikungu <111002283+njerikungu@users.noreply.github.com>
2022-12-20 19:26:01 +0000

    defining square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Square."""
+
+
+class Square:
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size : The size of the new square."""
+        self.__size = size
