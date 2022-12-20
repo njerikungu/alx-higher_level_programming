ff31e9c78857be4bbd42d57908258bb3914c0d80
njerikungu <111002283+njerikungu@users.noreply.github.com>
2022-12-20 21:45:01 +0000

    defines square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,44 +1,58 @@
 #!/usr/bin/python3
+"""
+Module 5-square
+"""
+
+
 class Square:
-    """Defines a square by size"""
+    """
+    Square definition
+    """
+
     def __init__(self, size=0):
-        """ Initializes the square"""
+        """
+        Initializes square
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def area(self):
-        """ Returns the square are of object"""
-        return (self.__size * self.__size)
+        Attributes:
+            size (int): defaults to 0 if none
+        """
+        self.size = size
 
     @property
     def size(self):
-        """ Returns the size value"""
+        """"
+        Getter
+
+        Return's size
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
         """
-        if not isinstance(value, int):
+        Setter
+
+        Args:
+            value: sets size to value if int and >= 0
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
-    def my_print(self):
-        """ Method that prints a # square according
-        to the size value
+    def area(self):
         """
-        if not self.__size:
-            print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end='')
-                print()
+        Calculates area of square
+
+        Returns:
+            area
+        """
+        return (self.__size)**2
+
+    def my_print(self):
+        """
+        Prints square with #'s
+        """
+        print("\n".join(["#" * self.__size for rows in range(self.__size)]))
