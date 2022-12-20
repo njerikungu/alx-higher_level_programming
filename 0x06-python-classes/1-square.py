218611235416c37110987627210d4a13ec3561a7
njerikungu <111002283+njerikungu@users.noreply.github.com>
2022-12-20 19:50:43 +0000

    defines square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,9 @@ Defines a Square
 
 
 class Square:
-     """Represent a square."""
+    """Represent a square."""
 
-     def __init__(self, size):
+    def __init__(self, size):
         """Initialize a new Square.
         Args:
             size : The size of the new square."""
